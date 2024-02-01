13d987d6532a5409a4ae9c6bf226713c9e3b36e6
YiHuaLin <ivy418286@gmail.com>
2024-02-02 01:32:20 +0800

    feat: 新增/編輯/刪除/頁數元件化 / fix: 產品頁改載入components引用Modal

---
+++ src/components/Pagination.vue
@@ -0,0 +1,32 @@
+// 頁數
+<template>
+       <nav aria-label="Page navigation example">
+        <ul class="pagination">
+          <li class="page-item" :class="{disabled:!pages.has_pre}">
+            <a class="page-link" aria-label="Previous" @click="getProducts(pages.current_page-1)">
+              <span aria-hidden="true">&laquo;</span>
+              <!-- <span class="sr-only">Previous</span> -->
+            </a>
+          </li>
+          <li v-for="page in pages.total_pages" :key="{page}" class="page-item" :class="{active:page===pages.current_page}">
+            <a @click="getProducts(page)" class="page-link" >{{page}}</a>
+            </li>
+         
+
+          <li class="page-item" :class="{disabled:!pages.has_next}">
+            <a class="page-link"  aria-label="Next" @click="getProducts(pages.current_page+1)">
+              <span aria-hidden="true">&raquo;</span>
+              <!-- <span class="sr-only">Next</span> -->
+            </a>
+          </li>
+        </ul>
+      </nav>
+</template>
+
+<script>
+ export default {
+    props:['getProducts','pages']
+ }
+</script>
+
+    
--- src/views/ProductListView.vue
+++ src/views/ProductListView.vue
@@ -49,8 +49,10 @@
             </tr>
           </tbody>
         </table>
+          <!-- 頁數 -->
+          <pagination :getProducts="getProducts" :pages="pages"></pagination>
         <p>
-          目前有 <span>{{ Object.values(products)?.length }}</span> 項產品
+         第{{pages.current_page}}頁有 <span>{{ Object.values(products)?.length }}</span> 項產品
         </p>
       </div>
       <div class="col-md-12">
@@ -82,148 +84,22 @@
       </div>
     </div>
 
-    <!-- Modal -->
-    <div id="productModal" ref="productModal" class="modal fade" tabindex="-1" aria-labelledby="productModalLabel"
-      aria-hidden="true">
-      <div class="modal-dialog modal-xl">
-        <div class="modal-content border-0">
-          <div :class="[`modal-header text-white ${isNew===false?'bg-primary':'bg-success'}`]">
-            <h5 id="productModalLabel" class="modal-title" >
-              <span v-if="isNew">新增產品</span>
-              <span v-else>編輯產品</span>
-            </h5>
-            <button @click="tempProduct={}" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            <div class="row">
-              <div class="col-sm-4">
-                <div class="mb-3">
-                  <label for="imageUrl" class="form-label">主要圖片</label>
-                  <input id="imageUrl" v-model="tempProduct.imageUrl" type="text" class="form-control mb-2" placeholder="請輸入圖片連結">
-                  <img class="img-fluid" :src="tempProduct.imageUrl">
-                </div>
-                <h3 class="mb-3">多圖新增</h3>
-                <div v-if="Array.isArray(tempProduct.imagesUrl)">
-                  <div class="mb-1" v-for="(image, key) in tempProduct.imagesUrl" :key="key">
-                    <div class="mb-3">
-                      <label :for="`imagesUrl${key}`" class="form-label">圖片網址{{key+1}}</label>
-                      <input :id="`imagesUrl${key}`" v-model="tempProduct.imagesUrl[key]" type="text" class="form-control"
-                        placeholder="請輸入圖片連結">
-                    </div>
-                    <img class="img-fluid" :src="image">
-                      <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.imagesUrl.splice(key,1)">
-                      刪除圖片 {{key+1}}
-                    </button>
-                  </div>
-                  <div v-if="!tempProduct.imagesUrl.length || tempProduct.imagesUrl[tempProduct.imagesUrl.length - 1]">
-                    <button class="btn btn-outline-primary btn-sm d-block w-100"
-                      @click="tempProduct.imagesUrl.push('')">
-                      新增圖片
-                    </button>
-                  </div>
-                  <!-- <div v-else>
-                    <button class="btn btn-outline-danger btn-sm d-block w-100" @click="tempProduct.imagesUrl.pop()">
-                      刪除圖片
-                    </button>
-                  </div> -->
-                </div>
-                <div v-else>
-                  <button class="btn btn-outline-primary btn-sm d-block w-100" @click="createImages">
-                    新增圖片
-                  </button>
-                </div>
-              </div>
-              <div class="col-sm-8">
-                <div class="mb-3">
-                  <label for="title" class="form-label">標題</label>
-                  <input id="title" v-model="tempProduct.title" type="text" class="form-control" placeholder="請輸入標題">
-                </div>
+    <!-- Modal Edit add  -->
+    <edit-product-modal 
+      :tempProduct="tempProduct" 
+      :updateProduct="updateProduct"
+      ref="modalEdit"
+      :isNew="isNew"
+      >
+    </edit-product-modal>
 
-                <div class="row">
-                  <div class="mb-3 col-md-6">
-                    <label for="category" class="form-label">分類</label>
-                    <input id="category" v-model="tempProduct.category" type="text" class="form-control"
-                      placeholder="請輸入分類">
-                  </div>
-                  <div class="mb-3 col-md-6">
-                    <label for="unit" class="form-label">單位</label>
-                    <input id="unit" v-model="tempProduct.unit" type="text" class="form-control" placeholder="請輸入單位">
-                  </div>
-                </div>
-
-                <div class="row">
-                  <div class="mb-3 col-md-6">
-                    <label for="origin_price" class="form-label">原價</label>
-                    <input id="origin_price" v-model.number="tempProduct.origin_price" type="number" min="0"
-                      class="form-control" placeholder="請輸入原價">
-                  </div>
-                  <div class="mb-3 col-md-6">
-                    <label for="price" class="form-label">售價</label>
-                    <input id="price" v-model.number="tempProduct.price" type="number" min="0" class="form-control"
-                      placeholder="請輸入售價">
-                  </div>
-                </div>
-                <hr>
-
-                <div class="mb-3">
-                  <label for="description" class="form-label">產品描述</label>
-                  <textarea id="description" v-model="tempProduct.description" type="text" class="form-control"
-                    placeholder="請輸入產品描述">
-                  </textarea>
-                </div>
-                <div class="mb-3">
-                  <label for="content" class="form-label">說明內容</label>
-                  <textarea id="content" v-model="tempProduct.content" type="text" class="form-control"
-                    placeholder="請輸入說明內容">
-                  </textarea>
-                </div>
-                <div class="mb-3">
-                  <div class="form-check">
-                    <input id="is_enabled" v-model="tempProduct.is_enabled" class="form-check-input" type="checkbox"
-                      :true-value="1" :false-value="0">
-                    <label class="form-check-label" for="is_enabled">是否啟用</label>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button @click="tempProduct={}" type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-              取消
-            </button>
-            <button type="button" :class="[`btn ${isNew===false?'btn-primary':'btn-success'}`]" @click="updateProduct">
-              確認
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <div id="delProductModal" ref="delProductModal" class="modal fade" tabindex="-1"
-      aria-labelledby="delProductModalLabel" aria-hidden="true">
-      <div class="modal-dialog">
-        <div class="modal-content border-0">
-          <div class="modal-header bg-danger text-white">
-            <h5 id="delProductModalLabel" class="modal-title">
-              <span>刪除產品</span>
-            </h5>
-            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-            是否刪除
-            <strong class="text-danger">{{ tempProduct.title }}</strong> 商品(刪除後將無法恢復)。
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">
-              取消
-            </button>
-            <button type="button" class="btn btn-danger" @click="delProduct">
-              確認刪除
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
+    <!-- Modal Delete -->
+    <delete-product-modal 
+      :delProduct="delProduct"
+      :tempProduct="tempProduct"
+      ref="modalDel">
+    </delete-product-modal>
+  
     <!-- Modal -->
 
 
@@ -232,23 +108,32 @@
 
 <script>
 import { Modal } from "bootstrap"
+import Pagination from '../components/Pagination.vue';
+import EditProductModal from '../components/EditProductModal.vue'
+import DeleteProductModal from '../components/DeleteProductModal.vue'
 export default {
   name: "ProductListView",
+  components:{
+    Pagination,
+    EditProductModal,
+    DeleteProductModal
+  },
   data() {
     return {
         path: 'ivy-vue3', // 個人 API Path
         token: '',
         products: [],
         tempProduct: {},
-        modalAdd:null,
-        modalDel:null,
+        pages:{},
+        // modalAdd:null,
+        // modalDel:null,
         isNew:false,
     }
   },
   mounted(){
-    console.log(this.$refs);
-    this.modalAdd = new Modal(this.$refs.productModal);
-    this.modalDel = new Modal(this.$refs.delProductModal);
+    // console.log(this.$refs);
+    // this.modalAdd = new Modal(this.$refs.productModal);
+    // this.modalDel = new Modal(this.$refs.delProductModal);
     },
   created() {
     
@@ -267,12 +152,13 @@ export default {
       })
   },
   methods: {
-    getProducts(){
+    getProducts(page=1){ //query parameter
           this.$http
-        .get(`/api/${this.path}/admin/products/all`)
+        .get(`/api/${this.path}/admin/products?page=${page}`)
         .then((res) => {
-          console.log(res)
-          this.products = res.data.products
+          // console.log(res)
+          this.products = res.data.products;
+          this.pages = res.data.pagination;//頁數
         })
 
         //失敗
@@ -290,17 +176,17 @@ export default {
             imagesUrl:[]
           };
           this.isNew = true;
-          this.modalAdd.show();
+          this.$refs.modalEdit.openModal();
         }else if(action==='edit'){
             this.tempProduct = JSON.parse(JSON.stringify(product)); //{...product}; //
             if(!Array.isArray(this.tempProduct.imagesUrl)){
               this.tempProduct.imagesUrl = [];
             }
-            this.modalAdd.show();
+             this.$refs.modalEdit.openModal();
              this.isNew = false;
         }else if(action==='delete'){
            this.tempProduct = {...product};
-           this.modalDel.show();
+           this.$refs.modalDel.openModal();
 
         }
         
@@ -321,7 +207,7 @@ export default {
         .then((res) => {
           console.log(res);
           this.getProducts();
-          this.modalAdd.hide();
+           this.$refs.modalEdit.closeModal();
           this.tempProduct = {}
     
         })
@@ -341,7 +227,7 @@ export default {
         .then((res) => {
           console.log(res);
           this.getProducts();
-          this.modalDel.hide();
+          this.$refs.modalDel.closeModal();
           this.tempProduct = {}
     
         })
